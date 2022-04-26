63c12cb5fb3e891582cfcfc1280a81314872ff63
rhemney <bostoneochieng69@gmail.com>
2022-04-26 05:06:49 -0700

    This find the smallest and replace with -1

--- smallestnumberminusone.c
+++ smallestnumberminusone.c
@@ -4,35 +4,44 @@
 int smallest_number()
 {
 	int number_array[6];
-
+	
 	int i;
-
+	
 	int search_number;
-
+	
 	int smallest;
-
+	
+	int indexNumberArray;
+	 
 	printf("Please Enter number of the arrays");
-
+	
 	for (i=0; i<6; i++) // Allows The users to enter the values of array
 	{
 		scanf("%d", &number_array[i]);
 	}
-
+	
 	smallest = number_array[0];
-
+	
 	for (i=0; i < 6; i++) //This interates through the array to find the smallest
 	{
 		if (number_array[i] < smallest)
 		{
 			smallest = number_array[i];
-			smallest = smallest - 1;
+			indexNumberArray=i;
 		}
 	}
-
-	printf("\nthe smallest element is: %d",smallest);
-
+	number_array[indexNumberArray]=-1;
+	
+	printf("\n New array content is :\n");
+	
+	for (i = 0; i < 6; i++)
+	{
+		printf("\%d\t",number_array[i]);
+		
+	}
+	
 	return(0);
-
+	
 }
 
 int main ()
