844925ab5148069ab75b5d113c4f932b2b490218
rhemney <bostoneochieng69@gmail.com>
2022-06-25 12:18:57 -0700

    C programming files

---
+++ storageclass.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * The static variables holds their value between multiple function calls
+ */
+void sum()
+{
+        static int a = 10;
+        static int b = 24;
+        printf("%d %d \n",a,b);
+        a++;
+        b++;
+}
+void main()
+{
+        int i;
+        for(i = 0; i< 3; i++)
+        {
+        sum(); // The static variables holds their value between multiple function calls.
+}
+}
