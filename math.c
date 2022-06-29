83a9f93c758d558b2543eb01ec41494045875caf
rhemney <bostoneochieng69@gmail.com>
2022-06-29 03:31:25 -0700

    Math function in C

---
+++ math.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include <math.h>
+
+/**
+ * Ceil() function returns the smallest integer value greater than or equal to x
+ */
+int main()
+{
+    printf("\n%f",ceil(3.6));
+
+    printf("\n%f",ceil(3.3));
+
+    printf("\n%f",floor(3.6));
+
+    printf("\n%f",floor(3.2));
+
+    printf("\n%f",sqrt(16));
+
+    printf("\n%f",sqrt(7));
+
+    printf("\n%f",pow(2,4));
+
+    printf("\n%f",pow(3,3));
+
+    printf("\n%d", abs(-12));
+
+    return 0;
+
+}
